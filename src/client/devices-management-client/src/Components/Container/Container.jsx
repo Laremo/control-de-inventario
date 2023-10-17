14cb32921eea8cce48cda82ab5f0c74c0267359c
Laremo <memoo.lara12306@hotmail.com>
2023-10-17 00:19:39 -0600

    frontend navbar functionality

--- src/client/devices-management-client/src/Components/Container/Container.jsx
+++ src/client/devices-management-client/src/Components/Container/Container.jsx
@@ -1,9 +1,16 @@
+import styles from './Container.module.css';
 import Navbar from '../Navbar/Navbar';
+import { useState } from 'react';
 export default function Container() {
+  const [mode, setMode] = useState(0);
+
   return (
-    <>
-      <Navbar />
+    <div className={styles.main}>
+      <Navbar
+        mode={mode}
+        setMode={setMode}
+      />
       <h1>Container</h1>
-    </>
+    </div>
   );
 }
--- src/client/devices-management-client/src/Components/Navbar/Navbar.jsx
+++ src/client/devices-management-client/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,46 @@
+import { useState } from 'react';
 import styles from './Navbar.module.css';
 
-export default function Navbar() {
+export default function Navbar({ mode, setMode }) {
+  const [navHidden, setNavHidden] = useState(true);
+
+  const handleNavBarStatus = () => {
+    setNavHidden(!navHidden);
+  };
+
   return (
     <div className={styles.navContainer}>
       <header className={styles.title}>Control de Inventario</header>
-      <nav className={styles.navbar}>
-        <ul>
-          <li>Control de Dispositivos</li>
-          <li>Control de Usuarios</li>
-          <li>Control de Préstamos</li>
+
+      <button
+        onClick={handleNavBarStatus}
+        className={
+          navHidden ? styles.mobileNavToggle : styles.mobileClose
+        }></button>
+
+      <nav className={`${styles.navbar} ${navHidden ? '' : styles.navbarShow}`}>
+        <ul id='navigation'>
+          <li
+            className={(mode === 0 ? styles.active : '')}
+            onClick={() => {
+              setMode(0);
+            }}>
+            <span>Control de Dispositivos</span>
+          </li>
+          <li
+            className={(mode === 1 ? styles.active : '')}
+            onClick={() => {
+              setMode(1);
+            }}>
+            <span>Control de Usuarios</span>
+          </li>
+          <li
+            className={(mode === 2 ? styles.active : '')}
+            onClick={() => {
+              setMode(2);
+            }}>
+            <span>Control de Préstamos</span>
+          </li>
         </ul>
       </nav>
     </div>
